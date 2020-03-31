16378bc3e7c4f123788b26f9251ee2d3853a5638
Wei Xin Koh <e0201350@u.nus.edu>
2020-03-31 19:42:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, object, {key: value});
+  return Object.assign({}, object, {[key]: value});
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
